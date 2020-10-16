6e7547b61976e0a911de890f21eda916f3248a85
Pepijn Hillemans <pepijnhillemans@gmail.com>
2020-10-16 21:23:26 +0200

    added new keymap for reviung

--- keyboards/reviung41/keymaps/pepino/keymap.c
+++ keyboards/reviung41/keymaps/pepino/keymap.c
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-
+                        
 enum layer_names {
     _BASE,
     _LOWER,
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                     '-------------------------------------'
  */
   [_BASE] = LAYOUT_reviung41(
-    KC_ESC,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
+    KC_ESC,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
     KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  RSFT_T(KC_ENT),
+    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_MINS,
                                KC_LGUI, LT(_LOWER, KC_BSPC),    KC_SPC,   LT(_RAISE, KC_ENT),  KC_TAB
   ),
   
@@ -85,8 +85,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
   [_RAISE] = LAYOUT_reviung41(
     _______,     KC_1,     KC_2,     KC_3,     KC_4,      KC_5,                     KC_6,       KC_7,     KC_8,        KC_9,    KC_0, _______,
-    _______,  _______,  KC_MPRV,   KC_MPLY,  KC_NEXT,  KC_VOLUP,                 KC_LEFT,    KC_DOWN,    KC_UP,    KC_RIGHT, _______, _______,
-    _______,  _______,   KC_RGUI,  KC_RALT,  KC_CAPS,  KC_VOLDOWN,            KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT, _______, _______,
+    _______,  _______,  KC_MPRV,   KC_MPLY,  KC_MNXT,  KC__VOLUP,                 KC_LEFT,    KC_DOWN,    KC_UP,    KC_RIGHT, _______, _______,
+    _______,  _______,   KC_RGUI,  KC_RALT,  KC_CAPS,  KC__VOLDOWN,            KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT, _______, _______,
                                                _______,   _______,  _______,  _______,  _______
   ),
 /*
@@ -105,8 +105,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
   
   [_ADJUST] = LAYOUT_reviung41(
-    XXXXXXX,    KC_F1,   KC_F2,     KC_F3,    KC_F4,     KC_F5,              KC_F6,       F7,       F8,       F9,      F10,  XXXXXXX,
-    XXXXXXX,    RGB_VAI, RGB_SAI,  RGB_HUI, RGB_MOD,   RGB_TOG,            XXXXXXX,  XXXXXXX,  XXXXXXX,      F11,      F12,  XXXXXXX,
+    XXXXXXX,    KC_F1,   KC_F2,     KC_F3,    KC_F4,     KC_F5,              KC_F6,       KC_F7,       KC_F8,       KC_F9,      KC_F10,  XXXXXXX,
+    XXXXXXX,    RGB_VAI, RGB_SAI,  RGB_HUI, RGB_MOD,   RGB_TOG,            XXXXXXX,  XXXXXXX,  XXXXXXX,      KC_F11,      KC_F12,  XXXXXXX,
     XXXXXXX,   RGB_VAD, RGB_SAD,  RGB_HUD, RGB_RMOD,   XXXXXXX,            RESET,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
                                             _______,   _______,  XXXXXXX,  _______,  _______
   ),
--- keyboards/reviung41/rules.mk
+++ keyboards/reviung41/rules.mk
@@ -15,9 +15,9 @@ BOOTLOADER = atmel-dfu
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no        # Mouse keys
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes        # Console for debug
+CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
