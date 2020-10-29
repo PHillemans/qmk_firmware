adfcd19c8945a6fe06c5cfa76d4e503e7e8e5b0c
Pepijn Hillemans <pepijnhillemans@gmail.com>
2020-10-29 17:41:01 +0100

    Changed possition of Lalt and corrected other small mistakes

--- keyboards/reviung41/keymaps/pepino/keymap.c
+++ keyboards/reviung41/keymaps/pepino/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *       ,-------------------------------------------.          ,----------------------------------------.
  *       |        |   !   |  @   |  {   |  }   |  |  |          |       |     |      |      |      |  | \|
  *       |--------+-------+------+------+------+-----|          |-------+-----+------+------+------+-----|
- *       |        |   #   |  $   |  (   |  )   |  `  |          |    +  |  -  |  /   |  *   |  %   |  ' "|
+ *       |  LALT  |   #   |  $   |  (   |  )   |  `  |          |    +  |  -  |  /   |  *   |  %   |  ' "|
  *       |--------+-------+------+------+------+-----+          +-------+-----+------+------+------+-----|
  *       |        |   %   |  ^   |  [   |  ]   |  ~  |          |    &  |  =  |  ,   |  .   |  / ? | - _ |
  *       `----------------------+------+------+------+----------+-------+-----+------+-------------------'
@@ -64,8 +64,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
   [_LOWER] = LAYOUT_reviung41(
     _______,  KC_EXLM,  KC_AT,    KC_LCBR,  KC_RCBR,   KC_PIPE,            _______,  _______,  _______,  _______,  _______, KC_BSLS,
-    _______,  KC_HASH,   KC_DLR,  KC_LPRN,  KC_RPRN,    KC_GRV,            KC_PLUS,  KC_MINS,  KC_SLSH,  KC_ASTR,  KC_PERC, KC_QUOT,
-    _______,  KC_PERC,   KC_CIRC, KC_LBRC,  KC_RBRC,   KC_TILD,            KC_AMPR,  KC_END,   KC_COMM,  KC_DOT,  KC_SLSH,  KC_MINS,
+    KC_LALT,  KC_HASH,   KC_DLR,  KC_LPRN,  KC_RPRN,    KC_GRV,            KC_PLUS,  KC_MINS,  KC_SLSH,  KC_ASTR,  KC_PERC, KC_QUOT,
+    _______,  KC_PERC,   KC_CIRC, KC_LBRC,  KC_RBRC,   KC_TILD,            KC_AMPR,  KC_EQL,   KC_COMM,  KC_DOT,  KC_SLSH,  KC_MINS,
                                             _______,   _______,  KC_ENT,   _______,  _______
   ),
   
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_RAISE] = LAYOUT_reviung41(
     _______,     KC_1,     KC_2,     KC_3,     KC_4,      KC_5,                     KC_6,       KC_7,     KC_8,        KC_9,    KC_0, _______,
     _______,  _______,  KC_MPRV,   KC_MPLY,  KC_MNXT,  KC__VOLUP,                 KC_LEFT,    KC_DOWN,    KC_UP,    KC_RIGHT, _______, _______,
-    _______,  _______,   KC_RGUI,  KC_RALT,  KC_CAPS,  KC__VOLDOWN,            KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT, _______, _______,
+    _______,  _______,  _______,  _______,  KC_CAPS,  KC__VOLDOWN,            KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT, _______, _______,
                                                _______,   _______,  _______,  _______,  _______
   ),
 /*
